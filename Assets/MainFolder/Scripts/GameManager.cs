183fe54593520ca22bc4c57ff61a89bb986cdd45
OeAtsu <youri.charbonnel@outlook.fr>
2019-04-24 22:22:49 +0200

    Cerveau.exe dans ton cul, et bien profond

--- Assets/MainFolder/Scripts/GameManager.cs
+++ Assets/MainFolder/Scripts/GameManager.cs
@@ -17,9 +17,32 @@ public class GameManager : MonoBehaviour
 
     public bool[] currentModuleStable;
 
-    public float coolDown = 30f;
-    float timeStamp;
+    public float coolDownState = 30f;
+    float timeStampState;
 
+    public float coolDownNextEvent = 10f;
+    float timeStampNextEvent;
+
+    public float[] EventsProbabilities = new float[] { 0.0f, 0.0f, 0.0f };
+
+    /* Mémo
+     1: Rien
+     2: EvenementModule
+     3: Lumière
+    */
+
+    public float[] modulesEventsProbabilities = new float[] { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+
+    /* Mémo
+     1: Surcharge
+     2: Surchauffe
+     3: radiation
+     4: surcharge/surchauffe
+     5: surcharge/radiation
+     6: radiation/surchauffe
+     7: radiation/surcharge/surchauffe  
+    */
+    
     private void Awake()
     {
         if(instance == null)
@@ -37,7 +60,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         type = GameStates.Préparation;
-        timeStamp = Time.time + coolDown;
+        timeStampState = Time.time + coolDownState;
         FixEventProbabilities();
 
     }
@@ -47,11 +70,12 @@ public class GameManager : MonoBehaviour
     {
         WhichModulesIsStable();
 
-        if (Time.time >= timeStamp) // Si 30 secondes se sont écoulés
+        if (Time.time >= timeStampState) // Si 30 secondes se sont écoulés
         {
             if (type == GameStates.Préparation) // Après l'état préparation suit forcement l'état Paisible
             {
                 type = GameStates.Paisible;
+                timeStampNextEvent = Time.time + coolDownNextEvent;
             }
 
             else
@@ -60,7 +84,12 @@ public class GameManager : MonoBehaviour
             }
 
             FixEventProbabilities();
-            timeStamp = Time.time + coolDown;
+            timeStampState = Time.time + coolDownState;
+        }
+
+        if(Time.time >= timeStampNextEvent && type != GameStates.Préparation)
+        {
+            NextEvent();
         }
     }
 
@@ -70,20 +99,32 @@ public class GameManager : MonoBehaviour
         {
             case GameStates.Préparation:
                 Debug.Log("Preparation");
+                EventsProbabilities = new float[] { 0.0f, 0.0f, 0.0f };
+                modulesEventsProbabilities = new float[] { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+                coolDownNextEvent = 10f;
                 break;
             case GameStates.Paisible:
                 Debug.Log("Paisible");
+                EventsProbabilities = new float[] { 0.2f, 0.65f, 0.15f };
+                modulesEventsProbabilities = new float[] { 0.27f, 0.27f, 0.27f, 0.053f, 0.053f, 0.053f, 0.03f };
+                coolDownNextEvent = 10f;
                 break;
             case GameStates.Trépidant:
                 Debug.Log("Trépidant");
+                EventsProbabilities = new float[] { 0.1f, 0.75f, 0.15f };
+                modulesEventsProbabilities = new float[] { 0.15f, 0.15f, 0.15f, 0.15f, 0.15f, 0.15f, 0.1f };
+                coolDownNextEvent = 5f;
                 break;
             case GameStates.Intenable:
                 Debug.Log("Intenable");
+                EventsProbabilities = new float[] { 0.01f, 0.84f, 0.15f };
+                modulesEventsProbabilities = new float[] { 0.12f, 0.12f, 0.12f, 0.163f, 0.163f, 0.163f, 0.15f };
+                coolDownNextEvent = 3f;
                 break;
         }
     }
 
-    void WhichModulesIsStable() // Stoque dans CurrentBoolStable[] les modules actuelement stable
+    void WhichModulesIsStable() // Stoque dans la liste de bool CurrentModuleStable[] les modules actuelement stable
     {
             for (int i = 0; i<ModuleManager.instance.Modules.Length; i++)
             {
@@ -99,7 +140,7 @@ public class GameManager : MonoBehaviour
             }
     }
 
-    int NbModulesStable() // Compte le nombre de module stable depuis CurrentBoolStable[] 
+    int NbModulesStable() // Compte le nombre de module stable depuis la liste de bool CurrentModuleStable[] 
     {
         int resultat = 0;
         for (int i = 0; i <currentModuleStable.Length; i++)
@@ -131,5 +172,84 @@ public class GameManager : MonoBehaviour
                 break;
         }
     }
+
+    float EventPicker(float[] probs) //Prendre un evenement selon ses probabilitée d'apparition
+    {
+
+        float total = 0; 
+
+        foreach (float elem in probs) // nombre de probabilitées dans probs
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.value * total;  // valeur de 0 à 1 * l'addition de toutes les probabilitées du tableau
+
+        for (int i = 0; i < probs.Length; i++)
+        {
+            if (randomPoint < probs[i])
+            {
+                return i; // retourne l'evenement en question
+            }
+            else
+            {
+                randomPoint -= probs[i];
+            }
+        }
+        return probs.Length - 1;
+    }
+
+    void NextEvent() // Quel est le prochain evenement ?
+    {
+        switch (EventPicker(EventsProbabilities)) // es-ce : Rien ou un problème de module ou de lummière ?
+        {
+            case 0:
+                Debug.Log("Il ne se passe rien");
+                break;
+            case 1:
+                switch (EventPicker(modulesEventsProbabilities)) // Quel type de problème ?
+                {
+                    case 0: // surcharge
+
+                        break;
+
+                    case 1: // surchauffe
+
+                        break;
+
+                    case 2: // radiation
+
+                        break;
+
+                    case 3: // surcharge et surchauffe
+
+                        break;
+
+                    case 4: //surcharge et radiation
+
+                        break;
+
+                    case 5: // radiation et surchauffe
+
+                        break;
+
+                    case 6: // surcharge, surchauffe et radiation
+
+                        break;
+
+                    default:
+                        Debug.Log("Etat de module non listé dans les evenements de modules");
+                        break;
+                }
+                break;
+            case 2:
+                Debug.Log("Et la lumière fut");
+                break;
+            default:
+                Debug.Log("Evenement non listé dans l'eventPicker");
+                break;
+        }
+    }
+
 
 }
