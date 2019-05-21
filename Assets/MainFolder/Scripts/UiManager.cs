27bfb726ad26aa904d3b7213a3e2a93e2ef132a0
Lauphorn <rafael.deoliveira@outlook.fr>
2019-05-21 17:33:41 +0200

    UiManager sans pause mdrrr c'est quoi ce jeu je suis morrrrrr

--- Assets/MainFolder/Scripts/UiManager.cs
+++ Assets/MainFolder/Scripts/UiManager.cs
@@ -9,18 +9,12 @@ public class UiManager : MonoBehaviour
     public static UiManager instance = null;
 
     [Header("Text")]
-    public Text playPause;
     public Text tryAgain;
     public Text quit;
 
     [Header("Images")]
     public Image Background01;
     public Image Background02;
-    public Image Background03;
-
-    [Header("Boules")]
-    public bool isPlaying = false;
-    public bool isPaused = false;
 
     [Header("Int")]
     public int index = 0;
@@ -51,28 +45,6 @@ public class UiManager : MonoBehaviour
         GreenHighlight();
     }
 
-    public void Play()
-    {
-        isPlaying = true;
-        playPause.text = "  Pause";
-    }
-
-    public void Pause ()
-    {
-        isPaused = true;
-        playPause.text = "  Resume";
-    }
-
-    public void Resume()
-    {
-        isPaused = false;
-        Play();
-
-
-        /*playPause.text = "  Pause";
-        //isPaused = false;*/
-    }
-
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -85,7 +57,7 @@ public class UiManager : MonoBehaviour
 
     public void Up()
     {
-        if(index > 0 && stack == 4)
+        if(index > 0 && stack == 3)
         {
             index--;
         }
@@ -93,7 +65,7 @@ public class UiManager : MonoBehaviour
 
     public void Down()
     {
-        if(index < 2 && stack == 4)
+        if(index < 1 && stack == 3)
         { 
             index++;
         }
@@ -106,28 +78,10 @@ public class UiManager : MonoBehaviour
             switch (index)
             {
                 case 0:
-                    if(!isPlaying && !isPaused)
-                    {
-                        Play();
-                    }
-
-                    else if (isPlaying && !isPaused)
-                    {
-                        Pause();
-                    }
-
-                    else if (isPlaying && isPaused)
-                    {
-                        Resume();
-                    }
-                    Debug.Log("selection ppr");
-                    break;
-
-                case 1:
                     Restart();
                     break;
 
-                case 2:
+                case 1:
                     Quit();
                     break;
             }
@@ -153,20 +107,11 @@ public class UiManager : MonoBehaviour
         {
             Background02.enabled = false;
         }
-
-        if (index == 2)
-        {
-            Background03.enabled = true;
-        }
-        else
-        {
-            Background03.enabled = false;
-        }
     }
 
     public void Stacking()
     {
-        if (stack < 4)
+        if (stack < 3)
         {
             stack++;
         }
