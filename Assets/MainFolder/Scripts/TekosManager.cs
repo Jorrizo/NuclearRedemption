30fa90a04d31f6a78f1f57513c7348cebbe22c98
Destati <joanna.ehrhardt@e-artsup.net>
2019-06-21 12:25:33 +0100

    Raf, ramène ton pc il a un plus grand écran

--- Assets/MainFolder/Scripts/TekosManager.cs
+++ Assets/MainFolder/Scripts/TekosManager.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class TekosManager : MonoBehaviour
 {
-
+    public Animator anim;
+    public int random;
     public int bonusTekos = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        random = Random.Range(1, 4);
+
+        SetAnimation();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void SetAnimation()
+    {
+        switch (random)
+        {
+            case 1:
+                anim.SetBool("1", true);
+                break;
+            case 2:
+                anim.SetBool("2", true);
+                break;
+            case 3:
+                anim.SetBool("3", true);
+                break;            
+        }
     }
 }
